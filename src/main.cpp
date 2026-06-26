e4fd4550fdf89a384306c585b88d72ab29e950b9
N-Moran <nicolasmoran897@gmail.com>
2026-06-25 19:20:47 -0500

    Basic main menu added with loop and cases.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,38 @@
+#include <iostream>
+#include <string>
+#include <limits>
+#include "manager.h"
+
 int main() {
-    return 0;
+    Manager taskManager;
+
+    int choice = 0;
+
+    while (true) {
+        std::cout << "\n--- TODO LIST MANAGER ---\n";
+        std::cout << "1. Create Task\n";
+        std::cout << "2. View Task\n";
+        std::cout << "3. Update Task\n";
+        std::cout << "4. Delete Task\n";
+        std::cout << "5. Exit\n";
+        std::cout << "Enter your choice: ";
+
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
+                break;
+        }
+    }
 }
